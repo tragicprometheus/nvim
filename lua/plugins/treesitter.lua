6d36f639912275904923cda9cf8a72d42c08803c
Joe Trenshaw <joseph.trenshaw@geaerospace.com>
2026-04-28 08:36:33 -0400

    Add csharp to treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,10 +6,10 @@ return {
     local treesitter = require("nvim-treesitter")
     treesitter.setup()
 
-	treesitter.install = { "bash", "c", "css", "cpp", "go", "html", "java", "javascript", "json", "lua", "markdown", "markdown_inline", "python", "rust", "tsx", "typescript" },
+	treesitter.install = { "bash", "c", "csharp", "css", "cpp", "go", "html", "json", "lua", "markdown", "markdown_inline", "python", "rust", "tsx", "typescript" },
 
     vim.api.nvim_create_autocmd('FileType', {
-      pattern = { "bash", "c", "css", "cpp", "go", "html", "java", "javascript", "json", "lua", "markdown", "markdown_inline", "python", "rust", "tsx", "typescript" },
+      pattern = { "bash", "c", "css", "cpp", "csharp", "go", "html", "json", "lua", "markdown", "markdown_inline", "python", "rust", "tsx", "typescript" },
 
       callback = function()
         vim.treesitter.start()
